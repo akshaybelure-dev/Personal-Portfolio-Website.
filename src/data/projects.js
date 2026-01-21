739a1cdadf78932571e17763bdb6f43a684cf28b
Akshay Belure <abelure4@gmail.com>
2026-01-21 12:27:12 +0530

    changing project img path

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,30 +1,26 @@
+import bookLogo from "../assets/book-logo1.svg";
+import spotifyImg from "../assets/spotify.webp";
+import expenseImg from "../assets/expense.webp";
+
 const projects = [
   {
     title: "OpenShelf",
     description: "A modern online bookstore UI with filtering and clean UX.",
-    image: "/src/assets/book-logo1.svg",
+    image: bookLogo,
     link: "https://akshaybelure-dev.github.io/BookStore-Project/",
   },
   {
     title: "Spotify Clone",
     description:
       "A Spotify-inspired UI with playlists, responsive layout, and modern design patterns.",
-    image: "/src/assets/spotify.webp",
+    image: spotifyImg,
     link: "https://your-spotify-clone-link.com",
   },
   {
     title: "Expense Tracker",
     description: "Track expenses with categories and monthly summaries.",
-    image: "/src/assets/expense.webp",
+    image: expenseImg,
     link: "https://your-expense-project-link.com",
-  },
-  
-  {
-    title: "Spotify Clone",
-    description:
-      "A Spotify-inspired UI with playlists, responsive layout, and modern design patterns.",
-    image: "/src/assets/spotify.webp",
-    link: "https://your-spotify-clone-link.com",
   },
 ];
 
